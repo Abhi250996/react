9f73aa28c5eb268e430f7561dc5569c3cf0bf876
Abhishek Sharma <developer.abhishek25@gmail.com>
2025-01-06 22:43:31 +0530

    Updated package.json and vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/react/', // Replace <repository-name> with the name of your GitHub repository
+  build: {
+      outDir: 'dist', // Default output directory for the build
+  },
+});
