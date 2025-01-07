4ec8c247367ba76af7add729fcbaec22352587cf
abhishek.sharma <abhishek.sharma@jilit.co.in>
2025-01-07 13:57:53 +0530

    ....

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // If you want to create a 'build' folder
+  },
   plugins: [react()],
-  base: '/',
+  base: '/react/',
 })
