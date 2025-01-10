3a134b4764488b903b36adcead1638af3da7fac5
Abhishek Sharma <developer.abhishek25@gmail.com>
2025-01-10 21:49:36 +0530

    Frontend Complete

--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -4,6 +4,15 @@ import { Form } from "react-router-dom";
 export function SignUp() {
 
 
+        const {
+            register,
+            handleSubmit,
+            watch,
+            formState: { errors },
+          } = useForm()
+        
+          const onSubmit = (data) => console.log(data)
+    
     return (
         <>
 
@@ -15,8 +24,8 @@ export function SignUp() {
                         Sign Up
                     </h1>
 
-                    <form>
-                        <div className="mb-4">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-4">
                             <label className="block text-gray-700 mb-2" htmlFor="name">
                              Email:
                             </label>
@@ -25,7 +34,10 @@ export function SignUp() {
                                 id="name"
                                 placeholder="Enter your email"
                                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                            />
+                                {...register("email", { required: true })}
+
+                                />
+                                      {errors.email && <span>This field is required</span>}
                         </div>
                         <div className="mb-4">
                             <label className="block text-gray-700 mb-2" htmlFor="email">
@@ -36,7 +48,10 @@ export function SignUp() {
                                 id="email"
                                 placeholder="Enter password"
                                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                            />
+                                {...register("email", { required: true })}
+
+                                />
+                                      {errors.email && <span>This field is required</span>}
                         </div>
                         <div className="mb-4">
                             <label className="block text-gray-700 mb-2" htmlFor="email">
@@ -47,7 +62,10 @@ export function SignUp() {
                                 id="email"
                                 placeholder="Confirm Password"
                                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
-                            />
+                                {...register("email", { required: true })}
+
+                                />
+                                      {errors.email && <span>This field is required</span>}
                         </div>
                          
                         <button
